0292ccbf48d971f5968c723e68c5dcf89269cdfc
Riki Misawa <e235740@ie.u-ryukyu.ac.jp>
2023-12-11 19:55:10 +0900

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args){
-        String str = null;
-        System.out.println(str.length());
+        try{
+            String str = null;
+            System.out.println(str.length());
+        }catch(NullPointerException e){
+            System.out.println("値が入力されていません\n"+e.getMessage());
+        }
     }
 }
 
